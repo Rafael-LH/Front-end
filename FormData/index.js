cc9d2ff89418d9556c942f337990798956633cea
Rafael Lopez <rafael.iskoat@gmail.com>
2019-10-03 22:40:37 -0500

    Empezando con un formulario con express

---
+++ FormData/index.js
@@ -0,0 +1,9 @@
+(function(){
+    document.querySelector("#btn-login").onclick = e => {
+        e.preventDefault()    
+        let form = new FormData(document.querySelector('#form-login'))
+        
+        
+    }
+
+}())
