88142bb06d2bc66e801329cb4a525de01f8ef37d
Rafael Lopez <rafael.iskoat@gmail.com>
2019-10-07 00:28:32 -0500

    Casi terminado el formulario

--- FormData/index.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    document.querySelector("#btn-login").onclick = e => {
-        e.preventDefault()    
-        let form = new FormData(document.querySelector('#form-login'))
-        
-        
-    }
-
-}())
