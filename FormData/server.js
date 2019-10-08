e5959f85c9d70f122444ec5e9535bc45fc4ba9f8
Rafael Lopez <rafael.iskoat@gmail.com>
2019-10-07 23:11:18 -0500

    terminamos nuestro formulario utilizamos express para el backend

--- FormData/server.js
+++ FormData/server.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+const upload = multer({ dest: 'images/' });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve('index.html'))
-})
+  res.sendFile(path.resolve('index.html'));
+});
+app.post('/', upload.single('image'), (req, res) => {
+  console.log(req.body);
+  res.status(200);
+});
 
 app.listen(3005, () => {
-    console.log('App corriendo en el puerto 3005');
-})
+  console.log('App corriendo en el puerto 3005');
+});
