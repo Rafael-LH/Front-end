a7a844d805e7ec706e57f5b6b3d57d32b0abf534
Rafael Lopez <rafael.iskoat@gmail.com>
2019-10-15 22:54:11 -0500

    Casi terminado el tema de This

--- This/index.js
+++ This/index.js
@@ -1,8 +1,29 @@
-const object = {
-    container: document.querySelector("#showPokemon"),
-    renderPokemon: function(name){
+class API {
+    async fetch(id){
+        try {
+            const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+            const responseData = await data.url
+            const responseFetch = await fetch(responseData)
+            const response = await responseFetch.json()
+            let res = response.name[0].toUpperCase() + response.name.slice(1, response.name.length)
+            debugger
+                        
+        } catch (error) {
+            console.log(`Ha ocurrido algun error ${error}`);
+        }
+
+    }
+}
+const api = new API()
+api.fetch(14)
+
+class Pokemon {
+    container = document.querySelector("#showPokemon")
+    renderPokemon(name){
         this.container.textContent = `El nombre del pokémon es ${name}`   
     }
 }
 
-console.log(object.renderPokemon('Pikachu'));
+
+
+
