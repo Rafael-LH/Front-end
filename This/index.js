ce1268dae71aa1d0195682d0a59f0d44a2b09e7e
Rafael Lopez <rafael.iskoat@gmail.com>
2019-10-16 22:58:14 -0500

    This

--- This/index.js
+++ This/index.js
@@ -1,29 +1,62 @@
+const container = document.querySelector("#showPokemon")
+  
 class API {
+
+    BASE_HTTP = 'https://pokeapi.co/api/v2/pokemon/'    
     async fetch(id){
         try {
-            const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+            const data = await fetch(`${this.BASE_HTTP}${id}/`)
+            if(data.status === 200){
+                
             const responseData = await data.url
-            const responseFetch = await fetch(responseData)
-            const response = await responseFetch.json()
-            let res = response.name[0].toUpperCase() + response.name.slice(1, response.name.length)
-            debugger
+            let responseFetch = await fetch(responseData)
+
+            if(responseFetch.status === 200){
+
+                const response = await responseFetch.json()
+                const res = response.name[0].toUpperCase() + response.name.slice(1, response.name.length)
+               
+                setTimeout(()=>{
+                    container.textContent = `El nombre del pokémon es ${res}`   
+                }, 1000)
+
+            }else if(responseFetch.status === 404){
+                container.innerHTML = `<span class='warning'>Pokémon no encontrado ⚠︎</span>`   
+            }else{
+                container.innerHTML = `<span class='danger'>Ha ocurrido algun problema, intentalo mas tarde ☠︎</span>`   
+            }
+
+            }else if(data.status === 404){
+                container.innerHTML = `<span class='warning'>Pokémon no encontrado ⚠︎</span>`   
+            }else{
+                container.innerHTML = `<span class='danger'>Ha ocurrido algun problema, intentalo mas tarde ☠︎</span>`   
+            }
                         
         } catch (error) {
-            console.log(`Ha ocurrido algun error ${error}`);
+            container.innerHTML = `<span class='danger'>Ha ocurrido algun problema, intentalo mas tarde ☠︎</span>`   
+            // console.log(`Ha ocurrido algun error ${error}`); // solo descomentar para hacer testing
         }
-
     }
 }
-const api = new API()
-api.fetch(14)
 
-class Pokemon {
+ 
+
+class Pokemon extends API{
+    constructor(id){
+        super(id)
+        this.id = id
+    }
     container = document.querySelector("#showPokemon")
-    renderPokemon(name){
-        this.container.textContent = `El nombre del pokémon es ${name}`   
-    }
+    renderPokemon(){
+        container.textContent = `El nombre del pokémon es ${name}`   
+    }  
+    renderType(){
+        container.textContent = `El nombre del pokémon es ${type}`   
+    }  
 }
-
+const pikachu = new Pokemon
+pikachu.fetch(25);
+container.textContent = `Cargando...`   
 
 
 
