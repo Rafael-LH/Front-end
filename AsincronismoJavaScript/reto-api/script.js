ee2a329726c2cae4c1babf9d0e3e190c537f33b0
Rafael Lopez <rafael.iskoat@gmail.com>
2020-03-17 21:44:54 -0600

    Finalzado el curso de asincronismo con Javascript

--- AsincronismoJavaScript/reto-api/script.js
+++ AsincronismoJavaScript/reto-api/script.js
@@ -1,11 +1,15 @@
 
-async function getAllCharacters() {
+async function getAllCharacters(){
  const data = await fetch('https://rickandmortyapi.com/api/character/')
  return await data.json()
 }
-async function getFirstCharacter() {
+async function getFirstCharacter(){
  const getFirstCharacter = await fetch('https://rickandmortyapi.com/api/character/1')
  return await getFirstCharacter.json()
+}
+async function getDimensionFirstCharacter(resultAllCharacters){
+ const dimensionFirstCharacter = await fetch(`${resultAllCharacters.results[0].origin.url}`);
+ return await dimensionFirstCharacter.json();
 }
 
 (async function(){
@@ -13,9 +17,11 @@ async function getFirstCharacter() {
  const loading = document.querySelector("#loading")
  const container = document.querySelector("#content")
  const resultAllCharacters = await getAllCharacters()
+ // console.log(resultAllCharacters.results[0]);
+ 
  const resultFirstCharacter = await getFirstCharacter()
+ const resultDimensionFirstCharacter = await getDimensionFirstCharacter(resultAllCharacters)
  const image = new Image()
-// console.log(resultAllCharacters);
 
  image.src = resultFirstCharacter.image
  image.addEventListener('load', () => {
@@ -23,6 +29,7 @@ async function getFirstCharacter() {
   container.innerHTML = `<p>Total characters: ${resultAllCharacters.info.count}</p>`
   container.appendChild(image)
   container.innerHTML += `<p>First character: ${resultFirstCharacter.name}</p>`
+  container.innerHTML += `<p>${resultDimensionFirstCharacter.dimension}</p>`
  })
 
 }())
